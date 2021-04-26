721e9732d5e0f26564581db15bfb429fdb5ec35a
Bruno Korbar <bjuncek@gmail.com>
2021-04-26 12:37:47 +0100

    ce loss coefficent can now be an arg if you want

--- main.py
+++ main.py
@@ -172,6 +172,7 @@ def get_args_parser():
         help="giou box coefficient in the matching cost",
     )
     # * Loss coefficients
+    parser.add_argument("--ce_loss_coef", default=1, type=float)
     parser.add_argument("--mask_loss_coef", default=1, type=float)
     parser.add_argument("--dice_loss_coef", default=1, type=float)
     parser.add_argument("--bbox_loss_coef", default=5, type=float)
