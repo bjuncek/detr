222b7d7cd47905729aee3e145aa4077dfd0a1557
Bruno Korbar <bjuncek@gmail.com>
2021-05-23 20:05:09 +0100

    Adding support for MOT dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,6 +7,7 @@ from .CondensedMovies import build_detection as build_cmd_det
 from .CondensedMovies import build_character as calvin
 from .wider import build
 from .vggface2 import build as buildvgg
+from .mot_data import build_MOT
 
 
 def get_coco_api_from_dataset(dataset):
@@ -30,9 +31,10 @@ def build_dataset(image_set, args):
         return build(image_set, args)
     if args.dataset_file == "vggface2":
         return buildvgg(image_set, crop=args.crop)
+    if args.dataset_file == "MOT17":
+        return build_MOT(image_set)
     if args.dataset_file == "coco_panoptic":
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
-
         return build_coco_panoptic(image_set, args)
     raise ValueError(f"dataset {args.dataset_file} not supported")
